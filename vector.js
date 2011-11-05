79fff16662c372954d77cdad8f265055da5710b5
Omar Ziranhua <oziranhua@gmail.com>
2011-11-05 16:00:27 -0700

    documenting potential way of performing a semantic diff.

--- vector.js
+++ vector.js
@@ -1,6 +1,6 @@
 // Activate real time update only under these conditions
 if(window.location.pathname !== '/w/index.php' && mw.config.get('wgAction') === 'view'){
-	// scope in the tracking library
+	// scope in the socket library
 	jQuery.getScript('http://inkdroid.org:3000/socket.io/socket.io.js', function(){
 		// At all systems go...
 		jQuery( function() {
@@ -14,7 +14,7 @@ if(window.location.pathname !== '/w/index.php' && mw.config.get('wgAction') === 
 			// same server that served the page, remove the 
 			// string literal argument 'http://localhost:3000/';
 			// from the docs, it seems that the socket library
-			// defaults to that domain.
+			// defaults to that domain at connection time.
 			var socket = io.connect('http://localhost:3000/');
 
 			// Once a connection is established, the server application that listens for updates
@@ -32,8 +32,44 @@ if(window.location.pathname !== '/w/index.php' && mw.config.get('wgAction') === 
 				var msg = jQuery.parseJSON(data);
 				// extract the payload
 				var updated_html = msg.html;
+					  
+				// TODO: Here we can perform a diff between the updated markup and
+				// the current markup in the page. To extract the current html
+				// we can do something like: var current_html = jQuery("div.mw-content-ltr").html();
+				// once we have both versions, we need an algorithm that does specialized type of diff.
+				// One that takes into consideration that the diff being performed is being performed
+				// on html. This is necessary because when removing or adding pieces of the content 
+				// in the resulting html, the algorithm must be aware if the content changed, is content visible to the 
+				// user, so that it may add the necessary visible markers that signal the user 
+				// that the content has changed.
+				//	  
+				// Example of when the algorithm should not add markers:
+				// current html = '<a href="http://somelink/">My Link</a>'
+				// updated html = '<a href="http://someotherlink/">My Link</a>'
+				// result html =  '<a href="http://someotherlink/">My Link</a>'
+				//
+				// Example of when the algorithm should add markers (addition):
+				// current html = '<a href="http://somelink/">My Link</a>'
+				// updated html = '<a href="http://someotherlink/">My New Link</a>'
+				// result html  = '<a href="http://someotherlink/">My <ins>New</ins> Link</a>'
+				//
+				// Example of when the algorithm should add markers (deletion):
+				// current html = '<div>Too much text here</div>'
+				// updated html = '<div>text here<div>'
+				// result html  = '<div><del>Too much </del>text here</div>'
+					  
+				// Once we have markers in place, we can use jquery to perform visual actions
+				// that will make the user see the changes (such as remove all <del> tags 
+				// from the content div one second after the html is inserted so that the user
+				// may witness the deletion).
+				
+				// A good starting point would be http://code.google.com/p/google-diff-match-patch/
+				// A derivative of this work seems feasible.
+					  
 				// perform the update
 				jQuery("div.mw-content-ltr").html(updated_html);
+					  
+				// peform diff animation after a certain timeout
 			});
 		});
 	});
